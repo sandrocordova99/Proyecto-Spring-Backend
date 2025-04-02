d5ac57ae998412a51034f710ec3d443dcc0cb36e
suusuario <sandrocordova99@hotmail.com>
2025-04-02 11:02:09 -0500

    se agregan entidades y se le relacionan con usuarios

---
+++ src/main/java/com/examen/integrador/Entidades/Profesores.java
@@ -0,0 +1,28 @@
+package com.examen.integrador.Entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Profesores {
+
+    @Id
+    private String id;
+
+    @Column(length = 100)
+    private String grado;
+    
+    @MapsId   
+    @OneToOne() 
+    @JoinColumn(name = "usuario_id")
+    private Usuarios usuarios;
+
+}
