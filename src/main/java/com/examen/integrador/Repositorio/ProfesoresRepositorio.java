bc59f087dee4647c6bfd05b6ad85ade6229b143c
suusuario <sandrocordova99@hotmail.com>
2025-04-02 11:06:23 -0500

    se agregan los repositorios

--- src/main/java/com/examen/integrador/Repositorio/ProfesoresRepositorio.java
+++ src/main/java/com/examen/integrador/Repositorio/ProfesoresRepositorio.java
@@ -3,9 +3,9 @@ package com.examen.integrador.Repositorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.examen.integrador.Entidades.Alumnos;
+import com.examen.integrador.Entidades.Profesores;
 
 @Repository
-public interface AlumnosRepositorio extends JpaRepository<Alumnos, String>{
-    
+public interface ProfesoresRepositorio extends JpaRepository<Profesores, String> {
+
 }
